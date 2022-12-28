81c8bd53ab3745e4db53e02dfad9f2d101ae1995
go-compile <97609133+go-compile@users.noreply.github.com>
2022-12-28 21:26:01 +0000

    feat: add mozaic struct and hkdf derive digest

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use hkdf::Hkdf;
+use sha2::{Sha256, Digest};
+
+
 fn main() {
-    println!("Hello, world!");
+	let mut hasher = Sha256::new();
+	hasher.update("certificate contents would typically go here");
+	let fingerprint = hasher.finalize();
+
+	let moz = Mozaic::new(&fingerprint);
+
+	println!("Fingerprint: {:X?}", &fingerprint);
+	println!("HKDF'ed: {:X?}", &moz.data);
+}
+
+struct Mozaic {
+    data: [u8; 32],
+}
+
+impl Mozaic {
+    pub fn new(data: &[u8]) -> Self {
+
+		let mut moz = Mozaic{
+			data: [0u8; 32]
+		};
+
+		let hk = Hkdf::<Sha256>::new(None, &data);
+		hk.expand(&[0u8;0], &mut moz.data).expect("hkdf to provide 32 bytes");
+
+        return moz;
+    }
 }
